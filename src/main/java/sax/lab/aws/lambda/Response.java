2b292b5c30da0ea21c0cd44e119e65058dd948b0
Salvatore Di Salvo <salvatoreangelo.disalvo@mediolanum.it>
2018-06-26 15:44:48 +0200

    15:44

--- src/main/java/sax/lab/aws/lambda/Response.java
+++ src/main/java/sax/lab/aws/lambda/Response.java
@@ -4,9 +4,17 @@ package sax.lab.aws.lambda;
  * Created by SalvatoreAngelo.DiSa on 26/06/2018.
  */
 public class Response {
-    String message;
+    private String message;
 
     Response( String message ) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
         this.message = message;
     }
 }
